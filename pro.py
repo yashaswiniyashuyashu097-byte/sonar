b3472196dfdda5bab0295846b90d5ef7606568e2
yashaswiniyashuyashu097-byte <yashaswiniyashuyashu097@gmail.com>
2026-06-05 10:31:21 +0530

    Update pro.py

--- pro.py
+++ pro.py
@@ -1,34 +1,13 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+try:
+    # Put the code that might crash here
+    number = int(input("Enter a number: "))
+    result = 10 / number
+    print(f"Result is {result}")
+    
+except ZeroDivisionError:
+    # Runs if the user types 0
+    print("Error: You cannot divide by zero!")
 
-class CircularlyLinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = None
-
-    def insert(self, data):
-        node = Node(data)
-        if self.head is None:
-            node.next = node
-            self.head = node
-            self.tail = node
-        else:
-            node.next = self.head
-            self.tail.next = node
-            self.tail = node
-
-    def printCll(self):
-        temp = self.head
-        print(temp.data)
-        while(temp.next != self.head):
-            temp = temp.next
-            print(temp.data)
-
-# Example usage
-cll = CircularlyLinkedList()
-cll.insert(1)
-cll.insert(2)
-cll.insert3)
-cll.printCll()
+except ValueError:
+    # Runs if the user types words instead of numbers
+    print("Error: Please enter a valid number.")
